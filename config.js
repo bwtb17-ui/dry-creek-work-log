77ed3e0ed8c5555ef9a991a624103103d914f67a
bwtb17-ui <bwtb17@gmail.com>
2026-07-30 13:39:56 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Never paste an sb_secret_ key into this file.
 
 export const SUPABASE_URL = "https://avwyybhagbtfggoaicpb.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_LmBL3KUreZJpBvG-UwbvJA_JRLZyIvb";
